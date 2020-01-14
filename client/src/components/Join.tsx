3251dd4002310d65182b43078c6adf3788f8ae85
Christian-Aman <c.amanjohansson@gmail.com>
2020-01-14 19:50:29 +0100

    Client can connect with backend

--- client/src/components/Join.tsx
+++ client/src/components/Join.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import io from 'socket.io-client';
+
 import { updateConnection } from '../store/actions/chat';
 
 interface Props {}
 
+let socket;
+
 const Join: React.FC<Props> = () => {
-  return <div>Join!</div>;
+  const clickHandler = (): void => {
+    socket = io('localhost:5000');
+  };
+
+  return (
+    <div>
+      Join!
+      <input type='button' value='clickMEEE' onClick={clickHandler} />
+    </div>
+  );
 };
 
 export default connect(null, { updateConnection })(Join);
